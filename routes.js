489ca5d31d173e2c824ad96b2e12ffdc30430405
0nw <46507049+0nw@users.noreply.github.com>
2021-11-18 11:26:20 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,10 +5,10 @@ app.get('/', function (req, res) {
   res.sendFile("./views/index.html")
 })
 app.get('/contact', function (req, res) {
-  res.sendFile("./views/index.html")
+  res.sendFile("./views/contact.html")
 })
 app.get('/projects', function (req, res) {
-  res.sendFile("./views/index.html")
+  res.sendFile("./views/projects.html")
 })
 
 module.exports = app;
