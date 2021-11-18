524176a739a5ed9c14c56f36dc472011fe1daceb
0nw <46507049+0nw@users.noreply.github.com>
2021-11-18 17:54:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = require("./routes.js");
 var app = express();
-app.use(express.static('./views/css'))
+const express = require("express");
+var app = express();
+app.use(express.static(__dirname+'/views'))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+"/views/index.html")
+})
+app.get('/contact', function (req, res) {
+  res.sendFile(__dirname+"/views/contact.html")
+})
+app.get('/projects', function (req, res) {
+  res.sendFile(__dirname+"/views/projects.html")
+})
+
+
+app.listen(80);
+console.log("server started")
 
 
 app.listen(80);
