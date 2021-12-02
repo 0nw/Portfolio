00fa43181da287d556ad55092effcc35b5d2c718
0nw <46507049+0nw@users.noreply.github.com>
2021-12-02 11:06:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var app = express();
+var {config} = require("./data/config.json")
 app.use(express.static(__dirname+'/views'))
 app.get('/', function (req, res) {
   res.sendFile(__dirname+"/views/index.html")
@@ -12,5 +13,6 @@ app.get('/projects', function (req, res) {
 })
 
 
-app.listen(80);
-console.log("server started")
+app.listen(config.port);
+console.log("Server Started")
+console.log("Port: "+config.port)
